64eff795bdb4cd83c358b6d1c381ee88d97593b5
Lokeshgonnade <120356203+Lokeshgonnade@users.noreply.github.com>
2023-11-08 14:37:41 +0530

    Add files via upload

---
+++ scr/App.js
@@ -0,0 +1,10 @@
+import Todolist from "./components/Todolist";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Todolist/>
+    </div>
+  );
+}
